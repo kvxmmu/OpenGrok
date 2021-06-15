a7df8cd399b22d2ee72396f9264ab825c44a614b
Eblan <linus@linus.ru>
2021-06-15 16:59:00 +0300

    Add sigpipe ignore;

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 #include <loop/loop.hpp>
 #include <grokserver/server.hpp>
 
+#include <csignal>
+
 int main() {
+#ifdef SIGPIPE
+    signal(SIGPIPE, SIG_IGN);
+#endif
+
     Config cfg(6567, INADDR_ANY,
                "FreeGrok/1.1a");
     FreeGrok server(cfg);
